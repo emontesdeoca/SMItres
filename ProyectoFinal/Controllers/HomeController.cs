8a94b706b54cea29032af371871c7e5c05992004
Maximo <mamodom@gmail.com>
2014-04-30 15:37:22 -0400

    changing requierermsadasds
    Conflicts:
    	ProyectoFinal/Controllers/HomeController.cs

--- ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -45,15 +45,30 @@ namespace ProyectoFinal.Controllers
             {
                 var recibido = JsonConvert.DeserializeObject<RootObject>(data);
 
+                if (!db.Patients.Any(model => model.Cedula == recibido.modeloDatos.Cedula))
+                {
+                    int edad = 0;
+                    var edadValida = int.TryParse(recibido.modeloDatos.Edad,out edad);
+                    db.Patients.Add(new PatientModel
+                    {
+                        Nombre = recibido.modeloDatos.NombrePaciente,
+                        Cedula = recibido.modeloDatos.Cedula,
+                        CreadoPor = "Paramédico",
+                        Edad = (edadValida) ? edad : 0,
+                        SeguroMedico = recibido.modeloDatos.SeguroMedico,
+                        Sexo = recibido.modeloDatos.Sexo
+                    });
+                    db.SaveChanges();
+                }
 
                 repo.InsertOrUpdate(recibido);
                 repo.Save();
             }
             catch (Exception err)
             {
-                return new JsonResult { Data = err.ToString() };
+                return new JsonResult { Data = err.ToString(),JsonRequestBehavior = JsonRequestBehavior.AllowGet};
             }
-            return new JsonResult { Data = "Eureka" };
+            return new JsonResult { Data = "Eureka",JsonRequestBehavior = JsonRequestBehavior.AllowGet};
         }
 
         [HttpGet]
@@ -119,10 +134,10 @@ namespace ProyectoFinal.Controllers
             return View(db.RootObjects.ToList());
         }
 
-        public JsonResult GetAmbulances() 
+        public JsonResult GetAmbulances()
         {
-            
-            
+
+
             var result = new JsonResult();
             result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
             var data = new
